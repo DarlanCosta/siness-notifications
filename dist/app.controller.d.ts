08ca369d4856b3649870c26a99698a19a14b68f1
Darlan Costa <darlandsv@gmail.com>
2024-07-25 15:29:02 -0300

    feat/integration

--- dist/app.controller.d.ts
+++ dist/app.controller.d.ts
@@ -1,6 +1,16 @@
-import { AppService } from './app.service';
+import { PrismaService } from "./prisma.service";
+import { CreateNotificationBody } from "./create-notification-body";
 export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
+    private readonly prisma;
+    constructor(prisma: PrismaService);
+    list(): import(".prisma/client").Prisma.PrismaPromise<{
+        id: string;
+        recipientId: string;
+        content: string;
+        category: string;
+        readAt: Date | null;
+        createdAt: Date;
+        updatedAt: Date;
+    }[]>;
+    create(body: CreateNotificationBody): Promise<void>;
 }
--- dist/app.controller.js
+++ dist/app.controller.js
@@ -8,16 +8,32 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const common_1 = require("@nestjs/common");
-const app_service_1 = require("./app.service");
+const prisma_service_1 = require("./prisma.service");
+const create_notification_body_1 = require("./create-notification-body");
+const node_crypto_1 = require("node:crypto");
 let AppController = class AppController {
-    constructor(appService) {
-        this.appService = appService;
+    constructor(prisma) {
+        this.prisma = prisma;
     }
-    getHello() {
-        return this.appService.getHello();
+    list() {
+        return this.prisma.notification.findMany();
+    }
+    async create(body) {
+        const { recipientId, content, category } = body;
+        await this.prisma.notification.create({
+            data: {
+                id: (0, node_crypto_1.randomUUID)(),
+                content: content,
+                category: category,
+                recipientId: recipientId,
+            },
+        });
     }
 };
 exports.AppController = AppController;
@@ -25,10 +41,17 @@ __decorate([
     (0, common_1.Get)(),
     __metadata("design:type", Function),
     __metadata("design:paramtypes", []),
-    __metadata("design:returntype", String)
-], AppController.prototype, "getHello", null);
+    __metadata("design:returntype", void 0)
+], AppController.prototype, "list", null);
+__decorate([
+    (0, common_1.Post)(),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [create_notification_body_1.CreateNotificationBody]),
+    __metadata("design:returntype", Promise)
+], AppController.prototype, "create", null);
 exports.AppController = AppController = __decorate([
-    (0, common_1.Controller)(),
-    __metadata("design:paramtypes", [app_service_1.AppService])
+    (0, common_1.Controller)("notifications"),
+    __metadata("design:paramtypes", [prisma_service_1.PrismaService])
 ], AppController);
 //# sourceMappingURL=app.controller.js.map
